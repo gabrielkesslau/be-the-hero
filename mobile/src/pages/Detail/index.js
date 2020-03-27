a0c8e339ddf96e54397e461be70d8b8ccf3e7b9b
Gabriel Kesslau <gabrielkesslau@gmail.com>
2020-03-27 18:29:10 -0300

    ajuste no backend mobile (description)

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -45,8 +45,8 @@ export default function Detail() {
                 <Text style={styles.incidentProperty, { marginTop: 0 }}>ONG:</Text>
                 <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf} </Text>
 
-                <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>{incident.title}</Text>
+                <Text style={styles.incidentProperty}>DESCRIÇÃO:</Text>
+                <Text style={styles.incidentValue}>{incident.description}</Text>
 
                 <Text style={styles.incidentProperty}>VALOR:</Text>
                 <Text style={styles.incidentValue}>
