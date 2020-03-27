71800e9078a71fe49c966c6813ce68465aae1968
Gabriel Kesslau <gabrielkesslau@gmail.com>
2020-03-27 19:09:47 -0300

    ajuste front-end

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -54,7 +54,7 @@ export default function Profile(){
         <img src={logoImg} alt="Be The Hero" />
         <span>Bem vinda, {ongName} </span>
 
-        <Link className="button" to="/incidents/new">Cadastar novo caso</Link>
+        <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         <button onClick={handleLogout} type="button" >
           <FiPower size={18} color="#E02041" />
         </button>
